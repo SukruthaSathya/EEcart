a4d843a754117f2e965ddafef9dd804112f2f83a
Sukrutha <sukruthasathya@gmail.com>
2022-03-14 20:32:10 +0530

    Responsive Home Completed

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -4,9 +4,7 @@ import './Product.css'
 function Product({item}) {
   return (
     <div className='prod-container'>
-        <div className="circle">
-
-        </div>
+        
         <img src={item.img} alt="" />
         <div className="prod-info">
             <i className='fa fa-shopping-cart	'></i>
