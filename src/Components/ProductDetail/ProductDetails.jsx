50443806998a79655e5c2d3b968a47ddaeb9247e
Sukrutha <sukruthasathya@gmail.com>
2022-03-17 16:02:19 +0530

    Responsive complete

--- src/Components/ProductDetail/ProductDetails.jsx
+++ src/Components/ProductDetail/ProductDetails.jsx
@@ -12,12 +12,11 @@ function ProductDetails() {
                 </div>
                 <div className="prod-InfoContainer">
                     <h1>Denim Jumpsuit</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                     </p>
                     <span>$ 20.0</span>
                     <div className="filter-container">
-                        <div className="prod-filter">
+                        <div className="prod-filter" id='prod-filter-color'>
                             <span>Color</span>
                             <div className="filter-color" id='black'>
                             </div>
@@ -31,7 +30,7 @@ function ProductDetails() {
                             <span>1</span>
                             <i className='fa fa-plus'></i>
                         </div>
-                        <div className="prod-filter">
+                        <div className="prod-filter" id='prod-filter-size'>
                             <span>Size</span>
                             <select name="" id="">
                                 <option value="">XS</option>
@@ -42,6 +41,7 @@ function ProductDetails() {
                             </select>
                         </div>
                     </div>
+                    <hr />
                     <div className="add-container">
 
                         <button className=''>ADD TO CART</button>
